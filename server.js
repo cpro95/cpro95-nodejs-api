27d275e31e5034b585351433b7567c8a41b02b77
cpro95 <cpro95@gmail.com>
2018-05-27 20:13:37 +0900

    minor change

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// import routes
 const index = require('./routes/index');
 const movies = require('./routes/movies');
 
