c536195c1df0407fbef5c3dbdce32d6644fdfa66
cpro95 <cpro95@gmail.com>
2018-08-02 16:58:18 +0900

    minor changed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
 // GET home page
 router.get('/', function(req, res, next) {
     res.send(`
         <h1>Welcome to my Movies DB API service</h1>
         <p>
-            This is operated by query driven API.
+            Operated by query driven API.
         </p>
         <p>
             /movies?query=%%
